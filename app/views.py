a2fc8e80e290a3c1256870f835994f5f2373326b
Yu Xuan Liu <wynd07@gmail.com>
2016-11-13 06:54:29 -0800

    generator

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from flask import Flask
 from flask import send_from_directory, make_response
 from flask import render_template, Response, request, redirect, url_for, send_file
@@ -6,9 +10,29 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+import math
+import os
+
+
+import tensorflow as tf
+
+from im2txt import configuration
+from im2txt import inference_wrapper
+from im2txt.inference_utils import caption_generator
+from im2txt.inference_utils import vocabulary
+from os import listdir
+from os.path import isfile, join
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "./tmp"
+
+g = tf.Graph()
+with g.as_default():
+    model = inference_wrapper.InferenceWrapper()
+    restore_fn = model.build_graph_from_config(configuration.ModelConfig(), "/home/andrewliu/im2txt/memesmodel/train")
+g.finalize()
+vocab = vocabulary.Vocabulary("/home/andrewliu/im2txt/data/memes/word_counts.txt")
+
 
 def draw_caption(img, text, top=False):
 	draw = ImageDraw.Draw(img)
@@ -21,6 +45,7 @@ def draw_caption(img, text, top=False):
 		w, h = draw.textsize(text, font=font)
 		s -= 1
 		if s <= 12: break
+        h += 5
 	#Draw the text multiple times in black to get the outline:
 	for x in range(-3, 4):
 		for y in range(-3, 4):
@@ -39,6 +64,8 @@ def index():
 def send_img():
 	return send_from_directory('./tmp', 'test.jpg')
 
+import numpy as np
+
 @app.route('/get_meme', methods=['GET', 'POST'])
 def get_memes():
 	args = request.args.to_dict()
@@ -47,11 +74,31 @@ def get_memes():
 	range1 = args['range']
 	range2 = args['range1']
 
+        imgs = listdir("./static/train/")
+        idx = np.random.choice(range(len(imgs)), 1)[0]
+        filename = "./static/train/" + imgs[idx]
+
+        with tf.Session(graph=g) as sess:
+            restore_fn(sess)
+            generator = caption_generator.CaptionGenerator(model, vocab)
+            with tf.gfile.GFile(filename, "r") as f:
+                image = f.read()
+            captions = []
+            while len(captions) == 0:
+                captions = generator.beam_search(sess, image, np.random.uniform(0.2, 1.2, 1)[0])
+            caption = captions[0]
+            sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
+            sentence = " ".join(sentence)
+            sentence = sentence.replace(" n't", "n't").replace(" 're", "'re").replace(" ?", "?").replace(" .", ".").replace(" 'm", "'m").upper().split(" ")
+
+            cap1 = sentence[:int(len(sentence)/2)]
+            cap2 = sentence[int(len(sentence)/2):]
+            #sentence = " ".join(sentence)
 	#put text here
-	sample_txt = "sample text 3"
+	sample_txt = sentence#"sample text 3"
 
 	#open image to use here
-	img = Image.open("./static/kronwall.jpg")
+	img = Image.open(filename)#"./static/kronwall.jpg")
 
 
 	# resize to height 300
@@ -61,12 +108,12 @@ def get_memes():
 	img = img.resize((wsize,baseheight), Image.ANTIALIAS)
 
 	#add caption
-	draw_caption(img, sample_txt, top=True)
-
+	draw_caption(img, " ".join(cap1), top=True)
+        draw_caption(img, " ".join(cap2), top=False)
 	#save to show. THE FILE NAME HERE WILL BE USED AGAIN AT SEND_IMG
 	img.save("./tmp/test.jpg")
 	#Response here is never used. Doesnt really matter
 	return Response("./tmp/test.jpg")
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0')
--- meme_scraper/scraper_parser.py
+++ meme_scraper/scraper_parser.py
@@ -2,9 +2,9 @@ import json
 import magic
 import mimetypes
 import os
-import urllib.request
+import urllib
 
-f = open('memes2.json', 'r')
+f = open('meme_scraper/memes.json', 'r')
 data = json.loads(f.read())
 f.close()
 
@@ -14,10 +14,22 @@ filenames = []
 counter = 1000000
 for meme in data:
 	filename = 'reddit/' + str(counter)
-	urllib.request.urlretrieve(meme['meme_url'], filename)
+	success = False
+	for i in range(3):
+		try:
+			urllib.urlretrieve(meme['meme_url'], filename)
+		except Exception as e:
+			print e, meme['meme_url']
+			continue
+		success = True
+		break
+	if not success:
+		continue
 	mime_type = magic.from_file(filename, mime=True)
 	extension = mimetypes.guess_extension(mime_type)
 	if extension in ['.jpeg', '.jpg', '.jpe', '.png']:
+		if extension == '.jpe':
+			extension = '.jpg'
 		os.rename(filename, filename + extension)
 
 		captions.append({'image_id': counter, 'caption': meme['title']})
@@ -26,10 +38,12 @@ for meme in data:
 	else:
 		os.remove(filename)
 
-f = open('captions.json', 'w+')
-f.write(json.dumps(captions))
-f.close()
+	if counter %100 == 0:
+		print counter
+		f = open('captions.json', 'w+')
+		f.write(json.dumps(captions))
+		f.close()
 
-f = open('filenames.json', 'w+')
-f.write(json.dumps(filenames))
-f.close()
+		f = open('filenames.json', 'w+')
+		f.write(json.dumps(filenames))
+		f.close()
